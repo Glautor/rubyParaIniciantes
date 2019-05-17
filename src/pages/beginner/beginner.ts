c07cb9045795e54f94c5859460dc2ca26a8e3044
Glauton <glautoncardoso@gmail.com>
2019-05-17 03:41:39 -0300

    size images adjusts

--- src/pages/beginner/beginner.ts
+++ src/pages/beginner/beginner.ts
@@ -12,6 +12,24 @@ export class BeginnerPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.items = [
+      { title: 'Imprimir mensagem em Ruby', expanded: false, lines: [
+        {type: 'comment', text: '# Imprime o conteúdo e pula uma linha'},
+        {type: 'code', text: 'puts "Olá Mundo!""'},
+        {type: 'comment', text: '# Imprime o conteúdo, mas não pula uma linha'},
+        {type: 'code', text: 'print "Olá Mundo!"'},
+        {type: 'comment', text: '# Chama o método inspect do elemento'},
+        {type: 'code', text: 'p "Olá Mundo!"'}
+      ]},
+      { title: 'Comentários', expanded: false, lines: [
+        {type: 'comment', text: '# Existem dois tipos de comentário em Ruby'},
+        {type: 'comment', text: '# -> Comentário de linha'},
+        {type: 'comment', text: "=begin"},
+        {type: 'comment', text: "Comentário de bloco"},
+        {type: 'comment', text: "segunda linha"},
+        {type: 'comment', text: "terceira linha"},
+        {type: 'comment', text: "esse tipo de comentário é pouco usado, a própria documentação do ruby utiliza apenas comentários com #"},
+        {type: 'comment', text: '=end'}
+      ]},
       { title: 'Declaração de Variaveis', expanded: false, lines: [
         {type: 'comment', text: '# Variáveis Locais'},
         {type: 'code', text: 'nome = "Luíza"'},
@@ -20,10 +38,33 @@ export class BeginnerPage {
         {type: 'comment', text: '# Variáveis de Instância'},
         {type: 'code', text: '@idade = 19'},
         {type: 'comment', text: '# Variáveis de Classe'},
-        {type: 'code', text: '@@cont = 28'},
-      ]},       
+        {type: 'code', text: '@@cont = 28'}
+      ]},
+      { title: 'Tipagem', expanded: false, lines: [
+        {type: 'comment', text: '# Apesar de ruby ser uma linguagem altamente tipada, ela abstrai muito isso do usuário, sendo assim, não é necessário declarar variáveis para poder usálas'},
+        {type: 'code', text: 'idade = 12'},
+        {type: 'code', text: 'nome = "Adriana"'},
+        {type: 'comment', text: '# por meio do método .class podemos saber a qual classe nossa variável pertence'},
+        {type: 'code', text: 'puts idade.class'},
+        {type: 'comment', text: '=> Integer'},
+        {type: 'code', text: 'puts nome.class'},
+        {type: 'comment', text: '=> String'}
+      ]},
+      { title: 'Tags Ruby', expanded: false, lines: [
+        {type: 'comment', text: '# aqui declaramos a variável'},
+        {type: 'code', text: '<% @nome = "Glauton" %>'},
+        {type: 'comment', text: '# aqui apresentamos a variável ao usuário'},
+        {type: 'code', text: '<%= @nome %>'},
+        {type: 'comment', text: '# Obs.: para fins didáticos declaramos a variável na view, mas isso não é considerado boa prática, normalmente essa declaração é feita no controller e acessada na view.'}
+     ]},
+     { title: 'Métodos', expanded: false, lines: [
+       {type: 'code', text: 'def  novo_metodo'},
+       {type: 'comment', text: '# código ruby'},
+       {type: 'code', text: 'end'},
+       {type: 'comment', text: '# Quando seu método não possui conteúdo é possível declará-lo dessa forma'},
+       {type: 'code', text: 'def novo_metodo end;'}
+    ]}, 
     ];
-    console.log(this.items);
   }
 
   ionViewDidLoad() {
